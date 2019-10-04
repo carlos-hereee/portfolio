468fb77d176b3048a8bbfda946ba5750b3625d14
ch04937 <97hernandez.c@gmail.com>
2019-10-04 16:20:31 -0700

    fixed routing

--- src/App.js
+++ src/App.js
@@ -7,19 +7,21 @@ import AboutMe from './components/AboutMe';
 import ContactMe from './components/ContactMe';
 import Work from './components/Work'
 import Home from './components/Home';
+import Footer from './components/Footer'
 
 function App() {
   return (
     <div className='App'>
       <h2>Hello, I'm Carlos Hernandez.</h2>
       <h2>I'm a full-stack web developer.</h2> 
+      <p>This is a showcase of my projects and my abilities</p>
       
       <Navigation />
-      <Route exact path = '/home' compontent ={Home} />
-      <Route path = '/work' compontent ={Work} />
-      <Route path = '/about-me' compontent ={AboutMe} />
-      <Route path = '/contact-me' compontent ={ContactMe} />
-      
+      <Route exact path = '/home' component ={Home} />
+      <Route path = '/work' component ={Work} />
+      <Route path = '/about-me' component ={AboutMe} />
+      <Route path = '/contact-me' component ={ContactMe} />
+      <Footer />
     </div>
   );
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import './navigation.css'
 
 export default function Navigation() {
     return(
-        <div className='navigation'>
+        <div className='footer'>
             <Link to='/home' >
                 <p>Home</p>
             </Link>
