d304840c7c12f1b6ff847b489fe0aa2ad1f2528a
ch04937 <97hernandez.c@gmail.com>
2019-10-21 16:59:15 -0700

    add skill list

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,10 +1,42 @@
 import React from 'react';
 
-export default function AboutMe() {
+import './skills.css'
+
+export default function Skills() {
     return(
         <div>
-            <div className='about-me-header' >
-                This are the skills I have
+            <div className='skills-header' >
+                <div className='skills-list'>
+                    <p>Front End Languages and Frameworks</p>
+                    <ul> 
+                        <li>
+                            HTML
+                        </li>
+                        <li>
+                            CSS & SCSS
+                        </li>
+                        <li>
+                            JavaScript
+                        </li>
+                        <li>
+                            React
+                        </li>
+                        <li>
+                            Node
+                        </li>
+                    </ul>
+                </div>
+                <div className='skills-list'>
+                    <p>Back-end Languages and Frameworks</p>
+                    <ul>
+                        <li>
+                            Node
+                        </li>
+                        <li>
+                            Firebase
+                        </li>
+                    </ul>
+                    </div>
             </div>
         </div>
     )
