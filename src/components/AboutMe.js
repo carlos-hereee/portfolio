6058c3811c1a08aa0657cf041c62bc2ef6e27360
ch04937 <97hernandez.c@gmail.com>
2019-11-08 14:14:00 -0800

    mission statement

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -12,22 +12,17 @@ export default function AboutMe() {
             <div className='about-me-content'>
                 <img src={Pic} alt='temp-headshot'/>
                 <div className='about-me-content-para'>
-                    <h3>
-                        Carlos Hernandez
-                    </h3>
-                    <p>
-                        Always been passionate about coding, ever since I was in the 5th grade and I saw a computer for the first time. 
-                    </p>
-                    About Me 
-{/* Profile picture
-Professional
-Solo headshot 
-No hats, sunglasses, or distracting clothes/background
-Full name
-No nicknames or gamer tags
-Personal Mission Statement
-Mission Statement must be approved by Career Coach (you can use the summary you wrote in your Linkedin summary, for example) */}
-
+                    {/* Profile picture ✔ 
+                            Professional
+                            Solo headshot 
+                            No hats, sunglasses, or distracting clothes/background
+                        Full name ✔ 
+                            No nicknames or gamer tags
+                        Personal Mission Statement 
+                            Mission Statement must be approved by Career Coach (you can use the summary you wrote in your Linkedin summary, for example) */}
+                    <h3> Carlos Hernandez </h3>
+                    <h4> Mission Statement </h4>
+                    <p>Have an insatible passion for learning new technologies and improving my skill.</p>
                 </div>
             </div>
         </div>
