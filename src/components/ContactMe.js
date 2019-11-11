fed9442d335620bb99774125e1aaacb771538197
ch04937 <97hernandez.c@gmail.com>
2019-11-11 12:24:42 -0800

    WIP

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -1,29 +1,31 @@
 import React from 'react';
-
+import { Formik } from 'formik'
 
 export default function ContactMe() {
     return(
         <div>
+            {
+            //  Contact form
+            // Email and phone number easily accessible, links to Linkedin & Github, medium (if you have one)
+            // DO NOT include your resume on your portfolio. Your resume should be individually tailored to any role, and you want to give people a reason to get in touch with you. If you give it all away at once, they might be less inclined to reach out. 
+            
+            // form 
+            // name
+            // email
+            // your message
+            }
             <h1>Contact</h1>
             <p>
                Have a question or want to work together?
             </p>
+            <Formik 
+                initialValues={{}}
+            >
+
+            </Formik>
             {/* <Form>
 
-            </Form>
- */}
- {
-    //  Contact form
-// Email and phone number easily accessible, links to Linkedin & Github, medium (if you have one)
-// DO NOT include your resume on your portfolio. Your resume should be individually tailored to any role, and you want to give people a reason to get in touch with you. If you give it all away at once, they might be less inclined to reach out. 
-
-
-// form 
-// name
-// email
-// your message
-}
-
+            </Form> */}
         </div>
     )
 }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,14 +4,15 @@ import React from 'react';
 export default function Projects() {
     return(
         <div>
+            {/* Projects
+                Links to at least 2 projects, with code easily accessible + deployed link
+                Best to use projects from Build Weeks, capstone project, and personal side projects
+                NO sprint challenges or projects with Lambda in the title  
+            */}
             <h1>Projects</h1>
             <p>
                 this is the Projects page
             </p>
-            {/* Projects
-Links to at least 2 projects, with code easily accessible + deployed link
-Best to use projects from Build Weeks, capstone project, and personal side projects
-NO sprint challenges or projects with Lambda in the title  */}
 
         </div>
     )
