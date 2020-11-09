b0c93fcfde5b0f6b4b358482cb8aaed9fae15479
ch04937 <97hernandez.c@gmail.com>
2020-11-09 14:17:11 -0600

    addding: twitter

--- src/pages/Navigation.js
+++ src/pages/Navigation.js
@@ -4,11 +4,17 @@ import { Link } from "react-router-dom";
 import styles from "../stylesheets/header_footer.module.scss";
 
 export default function Nav() {
-	return (
-		<div className={styles.nav}>
-			<Link to="/">Home</Link>
-			<Link to="/projects">Projects</Link>
-			<Link to="/contact-me">Contact</Link>
-		</div>
-	);
+  return (
+    <div className={styles.nav}>
+      <Link to="/" className={styles.link}>
+        Home
+      </Link>
+      <Link to="/projects" className={styles.link}>
+        Projects
+      </Link>
+      <Link to="/contact-me" className={styles.link}>
+        Contact
+      </Link>
+    </div>
+  );
 }
