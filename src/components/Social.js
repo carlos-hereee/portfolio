e8b75a4774f72706013b2a99508faee34c49a312
ch04937 <97hernandez.c@gmail.com>
2020-12-18 15:09:18 -0600

    feat: less white space

---
+++ src/components/Social.js
@@ -0,0 +1,18 @@
+import { Icon } from "semantic-ui-react";
+
+const Social = () => (
+  <div>
+    <a
+      href="https://www.linkedin.com/in/ch04937-carlos-hernandez/"
+      className="social__link">
+      <Icon name="linkedin" size="big" />
+    </a>
+    <a href="https://www.github.com/ch04937" className="social__link">
+      <Icon name="github" size="big" />
+    </a>
+    <a href="https://twitter.com/ch04937" className="social__link">
+      <Icon name="twitter" size="big" />
+    </a>
+  </div>
+);
+export default Social;
--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -1,26 +1,11 @@
 import React from "react";
-import { Icon } from "semantic-ui-react";
-
+import Social from "../components/Social";
 const Footer = () => (
-  <footer>
-    <div className="footer">
-      <div>
-        <p>Copyright © 2020</p>
-      </div>
-      <div>
-        <a
-          href="https://www.linkedin.com/in/ch04937-carlos-hernandez/"
-          className="footer__link">
-          <Icon name="linkedin" size="big" />
-        </a>
-        <a href="https://www.github.com/ch04937" className="footer__link">
-          <Icon name="github" size="big" />
-        </a>
-        <a href="https://twitter.com/ch04937" className="footer__link">
-          <Icon name="twitter" size="big" />
-        </a>
-      </div>
+  <footer className="footer">
+    <div>
+      <p>Copyright © 2020</p>
     </div>
+    <Social />
   </footer>
 );
 
