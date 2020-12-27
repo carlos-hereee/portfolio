b3c96669fa4d978fd458dd24cd575ca0eb4c964c
ch04937 <97hernandez.c@gmail.com>
2020-12-26 21:00:07 -0600

    feat: adding logo

--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -1,8 +1,11 @@
 import React from "react";
 import Social from "../components/Social";
+import Ch04937 from "./Ch04937";
+
 const Footer = () => (
   <footer className="footer">
-    <div>
+    <div className="footer__copyright">
+      <Ch04937 />
       <p>Copyright © 2020</p>
     </div>
     <Social />
