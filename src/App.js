a9fa2b1274a4c766d61e13d585e93fe5906a3bdd
ch04937 <97hernandez.c@gmail.com>
2021-08-27 13:16:49 -0700

    no header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ContactMe from "./pages/ContactMe";
-import Header from "./pages/Header";
 import Projects from "./pages/Project";
 import Footer from "./pages/Footer";
 import AboutMe from "./pages/About";
@@ -8,7 +7,6 @@ import Introduction from "./pages/Introduction";
 
 const App = () => (
   <div className="app">
-    <Header />
     <Introduction />
     {/* <AboutMe /> */}
     {/* <Projects />
