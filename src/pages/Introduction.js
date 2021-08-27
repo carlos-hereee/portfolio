02b3bc9a60f64810800e7662bceb3b79326b5dd2
ch04937 <97hernandez.c@gmail.com>
2021-08-27 13:12:42 -0700

    updated intro

--- src/pages/Introduction.js
+++ src/pages/Introduction.js
@@ -2,14 +2,14 @@ import Logo from "../components/Logo";
 
 const Introduction = () => {
   return (
-    <div>
-      <div className="introduction-content">
+    <section className="introduction">
+      <div className="introduction-header card-glass-small">
         <h1>Hello my name is Carlos Hernandez,</h1>
         <h1>I'm a</h1>
         <h1>Front End Web Developer</h1>
       </div>
       <Logo size="large" />
-    </div>
+    </section>
   );
 };
 export default Introduction;
