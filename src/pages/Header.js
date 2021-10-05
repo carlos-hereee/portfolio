222ba92efeb95018a9819d51056ed6f4a95bf115
ch04937 <97hernandez.c@gmail.com>
2021-10-04 17:01:22 -0700

    can download cv

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -1,46 +1,57 @@
 import { useState } from "react";
 import Logo from "../components/Logo";
+import resume from "../assets/CarlosHernandezResume.pdf";
 
 const Header = () => {
   const [active, setActive] = useState("");
+  const [burger, setBurger] = useState(false);
 
-  const downloadCV = () => {
-    console.log("download cv");
+  const handleClick = (tab) => {
+    setActive(tab);
+    setBurger(false);
   };
   return (
-    <div className="header">
-      <div className="header-logo">
+    <>
+      <div className="header-logo-hamburger" onClick={() => setBurger(!burger)}>
         <Logo />
       </div>
-      <nav className="navbar-nav">
-        <button className="nav-item" onClick={() => setActive("projects")}>
-          <a
-            className={active === "projects" ? `nav-link-active` : "nav-link"}
-            href="#projects">
-            Projects
-          </a>
-        </button>
-        <button className="nav-item" onClick={() => setActive("about")}>
-          <a
-            className={active === "about" ? `nav-link-active` : "nav-link"}
-            href="#about">
-            About
-          </a>
-        </button>
-        <button className="nav-item" onClick={() => setActive("contact")}>
-          <a
-            className={active === "contact" ? `nav-link-active` : "nav-link"}
-            href="#contact-me">
-            Contact Me
-          </a>
-        </button>
-        <button className="nav-item" onClick={() => downloadCV()}>
-          <a className="nav-link" href="#Download-CV">
-            DownLoad CV
-          </a>
-        </button>
-      </nav>
-    </div>
+      <div className={burger ? `header-active` : "header"}>
+        <div className="header-logo">
+          <Logo />
+        </div>
+        <nav className="navbar-nav">
+          <button className="nav-item" onClick={() => handleClick("projects")}>
+            <a
+              className={active === "projects" ? `nav-link-active` : "nav-link"}
+              href="#projects">
+              Projects
+            </a>
+          </button>
+          <button className="nav-item" onClick={() => handleClick("about")}>
+            <a
+              className={active === "about" ? `nav-link-active` : "nav-link"}
+              href="#about">
+              About
+            </a>
+          </button>
+          <button className="nav-item" onClick={() => handleClick("contact")}>
+            <a
+              className={active === "contact" ? `nav-link-active` : "nav-link"}
+              href="#contact-me">
+              Contact Me
+            </a>
+          </button>
+          <li className="nav-item">
+            <a
+              className="nav-link"
+              href={resume}
+              download="CarlosHernandezResume.pdf">
+              DownLoad CV
+            </a>
+          </li>
+        </nav>
+      </div>
+    </>
   );
 };
 export default Header;
